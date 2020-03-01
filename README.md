dd8330fd1cc87afd2d59dedd80650db2eaf4dfa9
Daniil Chervyakov <2yodi@mail.ru>
2020-03-02 04:59:09 +0500

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Start template for build Javascript project
+
+Everything you need for comfortable coding is already collected in this template.
+
+- Babel
+- Eslint
+- Prettier
+- Webpack and Webpack-dev-server
+
+# How to Use
+
+Clone this repo
+
+```shell
+clone https://github.com/chervyakovru/javascript-start-template.git
+cd javascript-start-template
+npm i
+npm run dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
